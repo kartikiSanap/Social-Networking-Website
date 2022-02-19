67e4995de71a34ee94059e40e75f21aa39ba5b79
Kartiki Sanap <sanapkk123@gmail.com>
2022-02-20 04:16:10 +0530

    delete msg updated

--- delete_post.php
+++ delete_post.php
@@ -5,7 +5,7 @@ if(isset($_GET['post_id'])){
 	$delete_post="delete from posts where post_id=$post_id";
 	$run_delete=mysqli_query($con,$delete_post);
 	if($run_delete){
-		echo"<script>alert('A post have been deleted!')</script>";
+		echo"<script>alert('A post have been deleted successfully!')</script>";
 		echo"<script>window.open('home.php','_self')</script>";
 	}
 }
